af0c9448b9967e3b9a67fafc7cc0311ef6ded7b3
abul05 <ah0907@edu.turku.fi>
2023-10-10 08:43:23 +0300

    kokeilu

--- pages/ilmoittaudu.php
+++ pages/ilmoittaudu.php
@@ -1,33 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Ilmoittautuminen tapahtumaan</title>
+    <!-- Lisää tarvittavat meta-tagit ja tyylitiedostot täällä -->
 </head>
 <body>
-    <h1>Ilmoittautuminen tapahtumaan</h1>
+    <h1>Ilmoittautumislomake</h1>
 
-    <?php
-    // Tarkista, onko lomake lähetetty
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Tarkista, että ID on annettu
-        if (!empty($_POST["event_id"])) {
-            // Tässä voit lisätä koodin, joka käsittelee ilmoittautumista tietokantaan
-            $event_id = $_POST["event_id"];
-            // Voit lisätä tietokantaoperaatiot tässä
-
-            // Ohjaa käyttäjä "tapahtuma.php" -sivulle annetun event_id:n kanssa
-            header("Location: tapahtuma.php?id=" . $event_id);
-            exit(); // Lopeta tämän sivun suoritus
-        } else {
-            echo "<p>Anna tapahtuman ID ilmoittautuaksesi.</p>";
-        }
-    }
-    ?>
-
-    <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-        <label for="event_id">Tapahtuman ID:</label>
-        <input type="text" name="event_id">
+    <form method="post" action="tallenna_ilmoittautuminen.php">
+        <label for="event_id">Tapahtuman tunniste:</label>
+        <input type="text" id="event_id" name="event_id" required><br>
+        <label for="participant_id">Osallistujan tunniste:</label>
+        <input type="text" id="participant_id" name="participant_id" required><br>
         <input type="submit" value="Ilmoittaudu">
     </form>
 </body>
--- pages/osallistuja.php
+++ pages/osallistuja.php
@@ -1,34 +1,89 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Ilmoittautuminen tapahtumaan</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tietokannasta tiedot</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            color: white; /* Valkoinen teksti */
+            margin: 0;
+            padding: 0;
+        }
+
+        h1 {
+            text-align: center;
+            color: black;
+        }
+
+        table {
+            width: 60%;
+            margin: 20px auto;
+            border-collapse: collapse;
+            background-color: white; /* Valkoinen tausta taulukolle */
+        }
+
+        table, th, td {
+            border: 1px solid black;
+        }
+
+        th, td {
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #333; /* Tumma tausta otsikolle */
+            color: white; /* Valkoinen teksti otsikolle */
+        }
+
+        tr:nth-child(even) {
+            background-color: #666; /* Tumma tausta parillisille riveille */
+        }
+
+        tr:nth-child(odd) {
+            background-color: #444; /* Tumma tausta parittomille riveille */
+        }
+    </style>
 </head>
 <body>
-    <h1>Ilmoittautuminen tapahtumaan</h1>
+    <h1>Tapahtuma ja Osallistuja ID</h1>
 
     <?php
-    // Tarkista, onko lomake lähetetty
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Tarkista, että ID on annettu
-        if (!empty($_POST["event_id"])) {
-            // Tässä voit lisätä koodin, joka käsittelee ilmoittautumista tietokantaan
-            $event_id = $_POST["event_id"];
-            // Voit lisätä tietokantaoperaatiot tässä
+    // Yhdistetään tietokantaan (korvaa tiedot omilla tiedoillasi)
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "events_manager";
 
-            // Ohjaa käyttäjä "tapahtuma.php" -sivulle annetun event_id:n kanssa
-            header("Location: tapahtuma.php?id=" . $event_id);
-            exit(); // Lopeta tämän sivun suoritus
-        } else {
-            echo "<p>Anna tapahtuman ID ilmoittautuaksesi.</p>";
-        }
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Tarkistetaan yhteys
+    if ($conn->connect_error) {
+        die("Yhteys epäonnistui: " . $conn->connect_error);
     }
+
+    // Suoritetaan SQL-kysely tietojen hakemiseksi (vaihtamalla sarakkeiden nimet)
+    $sql = "SELECT event_id, participant_id FROM events_participants";
+    $result = $conn->query($sql);
+
+    // Tarkistetaan, onko tuloksia
+    if ($result->num_rows > 0) {
+        // Tulostetaan tiedot HTML-taulukkoon
+        echo "<table>";
+        echo "<tr><th>Tapahtuma ID</th><th>Osallistuja ID</th></tr>";
+        while($row = $result->fetch_assoc()) {
+            echo "<tr><td>" . $row["event_id"]. "</td><td>" . $row["participant_id"]. "</td></tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "Ei tuloksia";
+    }
+
+    // Suljetaan tietokantayhteys
+    $conn->close();
     ?>
 
-    <form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
-        <label for="event_id">Tapahtuman ID:</label>
-        <input type="text" name="event_id">
-        <input type="submit" value="Ilmoittaudu">
-    </form>
 </body>
 </html>
